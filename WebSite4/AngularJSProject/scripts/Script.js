09d9646f8a82e60096f25ae28c5f25353d4df821
Venkata Samala <venkata_samala@outlook.com>
2017-11-23 14:54:17 -0500

    Update Scripts with creating module and controller and later using them in basic html page.

--- WebSite4/AngularJSProject/scripts/Script.js
+++ WebSite4/AngularJSProject/scripts/Script.js
@@ -1,1 +1,10 @@
-﻿
+﻿/// <reference path="angular.min.js" />
+
+//Create a module // you can say this as a "main" class.
+var myApp = angular.module("myModule", []);
+
+//Create a controller and Instalizing into the module
+
+myApp.controller("cntrl", function ($scope) {
+    $scope.Message = "My Message!!!";
+});

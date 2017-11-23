819434541ec44946be6312af0848dc9f240352ce
Venkata Samala <venkata_samala@outlook.com>
2017-11-23 16:17:51 -0500

    ng-repeat directive

--- WebSite4/AngularJSProject/scripts/Script.js
+++ WebSite4/AngularJSProject/scripts/Script.js
@@ -4,7 +4,16 @@
 
 //Chaining Controller to module itself
 var myApp = angular.module("myModule", [])
-.controller("cntrl", function ($scope) {
-        $scope.Message = "Hell ng";
-    });
+    .controller("cntrl",
+        function($scope) {
+            var std = [
+                { name: "India", Sports: [{ name: "Cricket" }, {name: "Hockey" }] },
+                { name: "USA", Sports: [{ name: "Football" }, {name: "Soccer" }] },
+                {
+                    name: "Brazil",
+                    Sports: [{ name: "Football" },{ name: "Vollyball" }]
+                }
+            ];
+            $scope.std = std;
+        });
 

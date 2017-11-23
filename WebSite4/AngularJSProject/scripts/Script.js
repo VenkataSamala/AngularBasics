989d6be2d9d4dcf6fa96dcfd9bd7d131934ec04e
Venkata Samala <venkata_samala@outlook.com>
2017-11-23 15:39:57 -0500

    Two way binding in Angular

--- WebSite4/AngularJSProject/scripts/Script.js
+++ WebSite4/AngularJSProject/scripts/Script.js
@@ -5,11 +5,6 @@
 //Chaining Controller to module itself
 var myApp = angular.module("myModule", [])
 .controller("cntrl", function ($scope) {
-        var shoes = {
-            name: "Air Max",
-            company: "Nike",
-            image: "/Images/AirMax.jpg"
-        };
-        $scope.shoes = shoes;
+        $scope.Message = "Hell ng";
     });
 

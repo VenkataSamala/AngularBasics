f77d92c705588e1fce63a314cfb242ea29488c87
Venkata Samala <venkata_samala@outlook.com>
2017-11-24 15:32:05 -0500

    Searching by multiple properties

--- WebSite4/AngularJSProject/scripts/Script.js
+++ WebSite4/AngularJSProject/scripts/Script.js
@@ -7,26 +7,25 @@ var myApp = angular.module("myModule", [])
     .controller("cntrl",
         function($scope) {
             var studentDetails = [
-                { sName: "Venkat", sBirth: new Date("October 17, 1992"), sGender: "Male", sSalary: 25500.50 },
-                { sName: "Sama", sBirth: new Date("November 17, 1994"), sGender: "Female", sSalary: 50500 },
-                { sName: "Sunny", sBirth: new Date("December 17, 1994"), sGender: "Male", sSalary: 20500 },
-                { sName: "Arun", sBirth: new Date("September 17, 1992"), sGender: "Male", sSalary: 75500.75 },
-                { sName: "Rita", sBirth: new Date("January 17, 1993"), sGender: "Female", sSalary: 100500.50 }
+                { sName: "Venkat",sCity:"Kansas City", sGender: "Male", sSalary: 25500.50 },
+                { sName: "Sama",  sCity:"Jersey City", sGender: "Female", sSalary: 50500 },
+                { sName: "Sunny", sCity:"Westborough", sGender: "Male", sSalary: 20500 },
+                { sName: "Arun",  sCity:"South Grafton", sGender: "Male", sSalary: 75500.75 },
+                { sName: "Rita",  sCity:"Frisco", sGender: "Female", sSalary: 100500.50 }
             ];
             $scope.studentDetails = studentDetails;
-            $scope.sortColumn = "sName";
-            $scope.reverseSort = false;
 
-            $scope.sortData = function(column) {
-                $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
-                $scope.sortColumn = column;
-            }
-
-            $scope.sortClass = function(column) {
-                if ($scope.sortColumn == column) {
-                    return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+            $scope.search = function(item) {
+                if ($scope.searchText == undefined) {
+                    return true;
+                } else {
+                    if (item.sName.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                        item.sCity.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
+                        return true;
+                    }
                 }
-                return "";
-            };
+                return false;
+            }
+            return true;
         });
 
